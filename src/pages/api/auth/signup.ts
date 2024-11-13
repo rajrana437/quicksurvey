e992936e0a68a6886064b1de0c5158b36e8e989d
rajrana437 <rajrana437@gmail.com>
2024-11-13 14:57:19 -0500

    Managed secret

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import connectDB from '@/lib/db';
 
-const JWT_SECRET: string = "your_secret_key";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!JWT_SECRET) {
   throw new Error('JWT_SECRET environment variable is not set');
